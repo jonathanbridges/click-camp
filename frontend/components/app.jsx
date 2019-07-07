620c7f411780e09e0348574166aa22296bdcfe14
jonathanbridges <jbridges7@gmail.com>
2019-07-07 16:54:11 -0700

    add footer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -28,12 +28,27 @@ const App = () => (
 
     {/* Static Footer */}
     <footer>
-      <section className="main-footer">
-        <div className="footer-content">
-          <div className="footer-left"></div>
-          <div className="footer-right"></div>
-        </div>
-      </section>
+      <div className="footer-wrapper">
+        <section className="main-footer">
+          <div className="footer-content">
+            <div className="footer-left">
+              <big>Hipcamp is everywhere you want to camp.</big>
+              <p>Discover unique experiences on ranches, nature preserves, farms, vineyards, and public campgrounds across the U.S. Book tent camping, treehouses, cabins, yurts, primitive backcountry sites, car camping, airstreams, tiny houses, RV camping, glamping tents and more.</p>
+            </div>
+            <div className="footer-right"></div>
+          </div>
+        </section>
+        <section className="footer-secondary">
+          <div className="footer-secondary-content">
+            <div className="footer-secondary-left">
+              <p>Made in California.</p>
+            </div>
+            <div className="footer-secondary-right">
+              <p>©2019 Jonathan Bridges</p>
+            </div>
+          </div>
+        </section>
+      </div>
     </footer>
   </div>
 );
