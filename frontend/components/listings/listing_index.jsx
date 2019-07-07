f12fa9e6f0f8a4dcba4d3dc2001d07c229bb246e
jonathanbridges <jbridges7@gmail.com>
2019-07-06 20:52:39 -0700

    add search bar with CSS styling to main index page

--- frontend/components/listings/listing_index.jsx
+++ frontend/components/listings/listing_index.jsx
@@ -11,11 +11,38 @@ class ListingIndex extends React.Component {
     let listings = this.props.listings.map(listing => <ListingIndexItem listing={listing} key={listing.id} />)
 
     return (
-      <div>
+      <div className="index">
+        <div className="home-wrapper">
+          <h2 className="home-title">Everywhere you want to camp.</h2>
+          <div className="home-subtext">
+            <big>Book unique camping experiences on over <strong>300,000</strong></big>
+            <br/>
+            <big>campsites, ranches, vineyards, public parks, and more.</big>
+          </div>
+        </div>
+        <div className="search-wrapper">
+          <form className="search-main">
+            <div className="listing-search-input">
+              {/* <span className=""></span> */}
+              <input type="search" name="listings" id="listings" autocomplete="off" spellcheck="false" dir="auto" placeholder="San Francisco" />
+            </div>
+            <div className="dates-btn">
+              <span className="calendar"></span>
+              <span className="value">Enter Dates </span>
+            </div>
+            <div className="categories-btn">
+              <span className="tent"></span>
+              <span className="value">All camping </span>
+              <span className="chevron"></span>
+            </div>
+            <button className= "search-btn" type="submit">Search</button>
+          </form>
+        </div>
+        <div>
         <ul>
           {listings}
         </ul>
-        {/* <Link to={`/events/new`}>New Event</Link> */}
+        </div>
       </div>
     );
   }
