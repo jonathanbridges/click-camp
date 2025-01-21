ebd91dfce33142f8d3b353d98150f42fd441e9a1
Jonathan Bridges <jbridges7@gmail.com>
2025-01-21 14:57:28 -0800

    Configure CORS for prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:5173', 'http://127.0.0.1:5173']
+    origins 'localhost:5173',
+            'http://127.0.0.1:5173',
+            'click-camp-web.onrender.com'
 
     resource '/api/v1/*',
       headers: :any,
